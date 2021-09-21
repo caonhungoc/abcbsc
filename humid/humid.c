6bc8afcf0c3f98d3196937a7ff19ab061d01ea8f
caonhungoc <caonhungoc1996@gmail.com>
2021-09-21 17:43:52 +0700

    fix wrong location

--- humid/humid.c
+++ humid/humid.c
@@ -1,3 +1,13 @@
 #include "i2c.h"
 
-float getHumidValue();
+float getHumidValue() 
+{
+    float humid = 0;
+    humid = i2c.read(0xaa);
+    return humid;
+}
+
+void resetSensor()
+{
+    i2c.write(0x00);
+}
--- humid/humid.h
+++ humid/humid.h
@@ -1,8 +1,4 @@
 #include "humid.h"
 
-float getHumidValue() 
-{
-    float humid = 0;
-    humid = i2c.read(0xaa);
-    return humid;
-}
+float getHumidValue();
+void resetSensor();
